1f9a9f09a9f9fe62ee7f76c6b0ebbfd6030581e9
AK Boon <112550451+advaitavs@users.noreply.github.com>
2026-03-07 11:25:38 +0530

    automation

--- frappe_ak/hooks.py
+++ frappe_ak/hooks.py
@@ -1,7 +1,7 @@
 app_name = "frappe_ak"
 app_title = "Frappe AK"
 app_publisher = "AK"
-app_description = "AKCOM Ledger and Automation for Frappe"
+app_description = "AKCOM Ledger for Frappe"
 app_email = "advait.k@swajal.in"
 app_license = "MIT"
 app_logo = "/assets/frappe_ak/images/ak_logo.svg"
@@ -9,15 +9,6 @@ required_apps = ["frappe"]
 
 # Setup
 after_install = "frappe_ak.setup.install.after_install"
-
-# Document Events
-doc_events = {
-    "*": {
-        "after_insert": "frappe_ak.dispatcher.engine.handle_event",
-        "on_update": "frappe_ak.dispatcher.engine.handle_event",
-        "on_submit": "frappe_ak.dispatcher.engine.handle_event",
-    }
-}
 
 # AKCOM Permissions
 has_permission = {
@@ -28,14 +19,4 @@ has_permission = {
 permission_query_conditions = {
     "AKCOM Person": "frappe_ak.akcom.doctype.akcom_person.akcom_person.get_permission_query_conditions",
     "AKCOM Ledger Entry": "frappe_ak.akcom.doctype.akcom_ledger_entry.akcom_ledger_entry.get_permission_query_conditions",
-}
-
-# Scheduled Tasks
-scheduler_events = {
-    "all": [
-        "frappe_ak.dispatcher.engine.run_cron_automations",
-    ],
-    "daily": [
-        "frappe_ak.dispatcher.engine.cleanup_old_logs",
-    ],
 }
